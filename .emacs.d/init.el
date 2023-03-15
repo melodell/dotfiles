ba45ca58ed6ad3d56eb8f22ac06d7bea9195a0ce
Melina O'Dell <melinaodell48@gmail.com>
2023-03-15 16:30:04 -0400

    emacs repos https

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -49,8 +49,8 @@
 ;; several publicly available repositories.
 (require 'package)
 (setq package-enable-at-startup nil)
-(add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/"))
-(add-to-list 'package-archives '("gnu" . "http://elpa.gnu.org/packages/"))
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/"))
+(add-to-list 'package-archives '("gnu" . "https://elpa.gnu.org/packages/"))
 (package-initialize)
 
 ;; Bootstrap 'use-package' and enable it.  Later, 'use-package- will
